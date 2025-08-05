f1bd495601297be8505bc30187bcd9fea7e02125
Priyanshi Kanpariya <Priyanshi Kanpariya>
2025-08-05 21:42:02 +0530

    add file

---
+++ Task2.java
@@ -0,0 +1,18 @@
+public class Task2 
+{
+    public static void main(String[]args)
+    {
+        int i=20;
+        do
+        {
+            if(i%2==1)
+            {
+                System.out.println(i);
+            }
+              i--;
+        }  
+        while(i>=1);
+        }
+    }
+    
+
