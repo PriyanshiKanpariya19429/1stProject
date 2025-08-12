cd8a35ea39cc07967ee7318de46a85d5b935db8a
Priyanshi Kanpariya <Priyanshi Kanpariya>
2025-08-12 23:30:38 +0530

    add file

---
+++ Arr.java
@@ -0,0 +1,14 @@
+public class Arr 
+{
+   public static void main(String[] args) 
+   {
+     int[]a= {10,20,30};
+     int i;
+     for(i=0;i<=2;i++)
+     {
+        System.out.println("array element="+a[i]);
+     }
+    
+   }
+     
+}
