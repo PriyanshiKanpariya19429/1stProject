7a30558c64387d1155e75e77ff7896e4f4c52b59
Priyanshi Kanpariya <Priyanshi Kanpariya>
2025-08-22 22:07:41 +0530

    add file

---
+++ con1.java
@@ -0,0 +1,14 @@
+public class con1
+{
+    con1(String name,int age)
+    {
+        System.out.println(name);
+        System.out.println(age);
+    }
+    public static void main(String[] args) 
+    {
+        con1 c1=new con1("Priyanshi",19);
+        
+    }
+    
+}
